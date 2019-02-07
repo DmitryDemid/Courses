2a4e6738604b7dae0c220bb5820542a8104e2112
DmitryDemid <47427798+DmitryDemid@users.noreply.github.com>
2019-02-07 20:53:04 +0300

    Second

---
+++ second/js/sec.js
@@ -0,0 +1,9 @@
+function changeBg(color) {
+    var color = '#' + color;
+    document.body.style.background = color;
+}
+
+function deleteCont(){
+    document.getElementById("body1").innerHTML = "";
+    
+}
